0fa18ba3763aa91f9cca02755cef191cfb314e72
asheshpaul <asheshpaul64@gmail.com>
2025-02-07 19:22:15 +0530

    Deploy Flutter Web to GitHub Pages

--- lib/domain/repositories/employee_repository.dart
+++
@@ -1,9 +0,0 @@
-
-import 'package:ems/domain/entities/employee_model.dart';
-
-abstract class IEmployeeRepository {
-  Future<void> saveEmployee(EmployeeModel employee);
-  Future<EmployeeModel?> getEmployee(String id);
-  Future<List<EmployeeModel>> getAllEmployees();
-  Future<void> deleteEmployee(String id);
-}
--- lib/presentation/cubits/date_picker_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'date_picker_cubit.dart';
-
-@freezed
-class DatePickerState with _$DatePickerState {
-  const factory DatePickerState.initial() = _Initial;
-
-  const factory DatePickerState.dateSelected(DateTime date, int selectedButton) = _DateSelected;
-}
